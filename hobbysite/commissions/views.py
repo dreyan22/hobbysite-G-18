0b9d786d2263defdc53262a6a80d878e401827c8
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-03-20 09:25:08 +0800

    Update models.py and views.py in app

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Commission
+
+
+class CommissionListView(ListView):
+    model = Commission
+    template_name = "commissions/commission_list.html"
+
+
+class CommissionDetailView(DetailView):
+    model = Commission
+    template_name = "commissions/commission_detail.html"
