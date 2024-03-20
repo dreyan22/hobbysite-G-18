e5f6cdadc5e8ade38ce2c4078b02dd555edc38df
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-03-20 10:02:21 +0800

    Updated admin.py and added commission_header.html

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -17,7 +17,7 @@ class Commission(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('commission:commission_detail', args=[self.pk])
+        return reverse('commissions:commission_detail', args=[self.pk])
 
 
 class Comment(models.Model):
