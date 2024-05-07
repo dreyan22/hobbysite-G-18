dde5ea48b447b214316c2f010c29c642e1527151
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-05-08 03:08:27 +0800

    Update models.py

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-#from user_management.models import Profile
+from user_management.models import Profile
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
@@ -24,7 +24,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True)
-    #owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
     description = models.TextField()
     price = models.DecimalField(max_digits=100, decimal_places=2)
     stock = models.IntegerField(default=0)
@@ -54,7 +54,7 @@ class Transaction(models.Model):
         ('Delivered', 'Delivered'),
     ]
 
-    #buyer = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    buyer = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     amount = models.IntegerField()
     status = models.CharField(max_length=20, choices=STATUS_CHOICES) #, default='On cart')
