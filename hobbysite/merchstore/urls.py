d9ad9e2c964e378daf4ec646cbae78241c839ef0
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-05-08 02:38:44 +0800

    Update urls.py

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, CartView, TransactionListView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('merchstore/items', ProductListView.as_view(), name='product_list'),
     path('merchstore/item/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('merchstore/item/add', ProductCreateView.as_view(), name='product_add'),
+    path('merchstore/item/<int:pk>/edit', ProductUpdateView.as_view(), name='product_edit'),
+    path('merchstore/cart', CartView.as_view(), name='cart'),
+    path('merchstore/transactions', TransactionListView.as_view(), name='transactions'),
 ]
 
 app_name = 'merchstore'
