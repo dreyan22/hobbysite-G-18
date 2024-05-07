b9e447bc18da6b70563dd9c2b4a7843ac4ddddcd
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-05-08 02:39:51 +0800

    update.urls.py

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('merchstore.urls', namespace="merchstore")),
+    path('user/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
