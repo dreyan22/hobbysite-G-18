ce2a19433dc54f9318bf10190440a9647ea0a113
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-05-08 19:34:26 +0800

    Update settings.py

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_REDIRECT_URL = 'merchstore:product_list'
+LOGOUT_REDIRECT_URL = 'merchstore:product_list'
