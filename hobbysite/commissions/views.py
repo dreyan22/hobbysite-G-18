e1d7d0293fe54f3bacdd7c63883b82e3c78e5d22
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-05-08 10:27:53 +0800

    Working login - logout no return

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from .models import Commission
 
@@ -8,8 +10,13 @@ from .models import Commission
 class CommissionListView(ListView):
     model = Commission
     template_name = "commissions/commission_list.html"
+    
+    logout_url = '/commissions/list'
 
 
-class CommissionDetailView(DetailView):
+class CommissionDetailView(LoginRequiredMixin, DetailView):
     model = Commission
     template_name = "commissions/commission_detail.html"
+
+    login_url = '/user/login/'
+
