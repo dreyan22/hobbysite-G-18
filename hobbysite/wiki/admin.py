6597ff0fd600c342bc307aff53700de497462387
mOnchhh <ryle.rodriguez@student.ateneo.edu>
2024-05-08 22:56:38 +0800

    Updated admin, models, views, and urls

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from .models import ArticleCategory, Article
+from .models import ArticleCategory, Article, Comment
 
-class ArticleInline(admin.TabularInline):
+class ArticleInline(admin.StackedInline):
     model = Article
 
 class ArticleAdmin(admin.ModelAdmin):
-    model = [ArticleInline]
+    inlines = [ArticleInline]
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'article', 'created_on')
+    list_filter = ('created_on')
+    search_fields = ['author_username', 'article_title']
 
 admin.site.register(ArticleCategory, ArticleAdmin)
 admin.site.register(Article)
+admin.site.register(Comment, CommentAdmin)
