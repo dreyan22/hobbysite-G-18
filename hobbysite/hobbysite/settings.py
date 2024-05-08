0b07a60ceb7b9467adceb3e8605e8850161d4060
mOnchhh <ryle.rodriguez@student.ateneo.edu>
2024-05-08 22:55:01 +0800

    Updated settings and urls

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wiki',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,9 +123,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/"static",
+]
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/wiki/articles'
+LOGOUT_REDIRECT_URL = '/wiki/articles'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
